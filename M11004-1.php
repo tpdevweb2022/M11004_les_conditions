453b552c059680f1c84ee8e0aa4c31d160e4f64e
anthonymerlier <webmaster@consor.fr>
2022-04-11 00:29:30 +0200

    ex1 bug fixes

--- M11004-1.php
+++ M11004-1.php
@@ -1,1 +1,4 @@
 <?php
+
+// Créer une variable age et l'initialiser avec une valeur.
+// Si l'age est supérieur ou égale à 18, afficher Vous êtes majeur. Dans le cas contraire, afficher Vous êtes mineur.
