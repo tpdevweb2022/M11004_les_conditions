ea76f5e0c572b73de726136fcbd87630f641d9f3
anthonymerlier <webmaster@consor.fr>
2022-04-11 00:16:12 +0200

    Ex1 ok

---
+++ M11004-1.php
@@ -0,0 +1,29 @@
+<?php
+
+$eleve = [
+    "prenom" => "Maxime",
+    "genre" => "M",
+    "taille" => 157,
+    "age" => 13,
+    "notes" => [
+        "Français" => [12, 14, 7, 11],
+        "Anglais" => [14, 11, 9, 18],
+        "Maths" => [7, 9, 11, 12]
+    ]
+];
+
+// Vérifions maintenant quelques conditions :
+//     Maxime est-il un garçon de moins de 17 ans ?
+
+//     Mesure t-il plus de 1,57m ?
+
+//     Maxime possède t-il une moyenne en Français supérieure à 11 ?
+
+//     Sa moyenne générale est-elle inférieur ou supérieure à 12 ?
+
+//     Maxime est-il un garçon de plus de 12 ans mesurant plus de 1,52m ?
+
+    
+//     Pour les plus courageux : (Facultatif)
+
+//     Si Maxime est un garçon qui fait plus de 1,50m et a 13 ans ou plus, il faut alors afficher la moyenne des matières Français et Anglais uniquement, et l'afficher avec une note moyenne sur un barème de 100 pts.
---
+++ README.md
@@ -0,0 +1,22 @@
+# M11004 - Les conditions
+
+## CONSIGNES
+
+Après avoir forké ou récupéré ce projet, ouvrez le avec VS Code ou votre éditeur préféré.
+
+### Exercice 1 : Fichier M11004-1.php
+
+#### Après avoir étudié le profil de Maxime
+
+Maxime est-il un garçon de moins de 17 ans ?
+Mesure t-il plus de 1,57m ?
+Maxime possède t-il une moyenne en Français supérieure à 11 ?
+Sa moyenne générale est-elle inférieur ou supérieure à 12 ?
+Maxime est-il un garçon de plus de 12 ans mesurant plus de 1,52m ?
+
+**Pour les plus courageux : (Facultatif\)**
+Si Maxime est un garçon qui fait plus de 1,50m et a 13 ans ou plus, il faut alors afficher la moyenne des matières Français et Anglais uniquement, et l'afficher avec une note moyenne sur un barème de 100 pts.
+
+### Exercice 2 : Fichier M11004-2.php
+
+####
